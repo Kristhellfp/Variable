49eaee73bd28e8a90bd5142db55e6d7b082028d4
Kristhellfp <159474506+Kristhellfp@users.noreply.github.com>
2025-01-14 18:37:49 -0600

    Add files via upload

---
+++ Variables/main.js
@@ -0,0 +1,40 @@
+
+
+//Variables 
+
+let edad = 18;
+let nombre = "Kriss";
+let libras = "1.5";
+
+
+
+
+//Declarar constante
+const PI = 3.1416;
+
+
+
+//Actividad Variables 
+let cantidad_alumnos = "21";
+let cantidad_sillas = "22";
+let altura_promedio = "1.65"
+let nombre_profesor = "Jossue";
+let edad_promedio = "17"
+let herramienta_base = "Computadora"
+let cantidad_lamparas = "4";
+let puntos_acceso = "2";
+let cantidad_mesas = "11";
+let materias_base = "6"
+
+
+//Actividad 1: Variables del problema 1 
+let tiempo = "10";
+let velocidad_inicial = "0";
+let velocidad_final = "30";
+
+
+
+//Actividad 1: Variables del problema 2
+let productos_existente = "150";
+let compras_cantidad = "50";
+let ventas_cantidad = "75";
